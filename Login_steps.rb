5576f1d3af5dcef3a17cbcc51749d74581c346e3
savitha K <savithak759@gmail.com>
2019-12-13 11:44:18 +0530

    secondscenario

--- Login_steps.rb
+++ Login_steps.rb
@@ -19,6 +19,26 @@ find(:xpath, './/span[text()="Next"][contains(@class, "RveJvd")]').click
 end
 
 
-Then("I should navigate to Inbox page") do
+Then("I should navigate to Inbox page" ) do
 expect(page).to have_text 'Primary'
+sleep 2
+end
+
+Given("I am on the compose page") do
+find(:xpath, './/div[text()="Compose"][contains(@class, "T-I J-J5-Ji T-I-KE L3")]').click
+end
+
+Given("I enter emailid") do
+fill_in('to', :with => 'kkirthy80@gmail.com') 
+sleep 3
+end
+
+Given("I enter subject") do
+fill_in('subjectbox', :with => 'Attach_file') 
+sleep 3
+end
+
+Given("I click on send button") do
+find(:xpath, './/div[text()="Send"]').click
+sleep 2
 end
--- logintoGmail.feature
+++ logintoGmail.feature
@@ -8,5 +8,9 @@ When I click on Next button
 Then I should navigate to Inbox page
 
 
-
+Scenario: Sending mail
+Given I am on the compose page
+And I enter emailid
+And I enter subject
+When I click on send button
     
